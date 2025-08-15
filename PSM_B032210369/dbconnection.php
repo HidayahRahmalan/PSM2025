b1be37033facb9e17bf09a83ae9e4e21a2076947
Nur Shahira Atilia binti Zainuddin <153926405+AtiliaZdean@users.noreply.github.com>
2025-08-15 15:37:05 +0800

    try upload

---
+++ PSM_B032210369/dbconnection.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "B032210369";
+$password = "hygieiahub";
+$dbname = "PSM_B032210369";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} /*else {
+    echo "Database connection successful!";
+} */
+?>
