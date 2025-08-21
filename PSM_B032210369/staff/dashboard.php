ac3d4442d1ce15590fdaafe5262c8179f69bd89a
Nur Shahira Atilia binti Zainuddin <153926405+AtiliaZdean@users.noreply.github.com>
2025-08-22 02:37:46 +0800

    Update dashboard.php

--- PSM_B032210369/staff/dashboard.php
+++ PSM_B032210369/staff/dashboard.php
@@ -129,12 +129,12 @@ if (!isset($_SESSION['staff_id'])) {
             </div>
           </li>
 
-          <!-- Maintenance -->
+          <!-- Maintenance
           <li class="nav-item">
             <a class="nav-link" href="maintenance.php">
               <span class="menu-title">Maintenance</span>
             </a>
-          </li>
+          </li> -->
         </ul>
       </nav>
 
